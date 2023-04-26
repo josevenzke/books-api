0ece37bd56da07ed9cca01eaa1884c1b8ac82f23
josevenzke <josevenzkejr@gmail.com>
2023-04-26 14:30:53 -0300

    refactor: removed unused endpoints

--- api/books/controller.py
+++ api/books/controller.py
@@ -1,3 +1,4 @@
+import json
 from api.config.app import app, response
 from api.books import services
 
@@ -8,27 +9,11 @@ class BooksResource:
         response.json = {"books":books}
         return response
     
-    def post(self,req):
-        print(req.json)
-        response.json = req.json
+    def post(self, request):
+        data = request.json
+        response.json = data
         return response
     
-@app.route("/books", methods=['GET','POST'])
-def home(request):
-    if request.method == 'GET':
-        books = Book.objects.get_all()
-        result = deserializer(books)
-        response.json = {"books":result}
-
-    elif request.method == 'POST':
-        books_data = [
-            {"name": "Pythoning", "lenght": 400, "genre": "tech"},
-            {"name": "Harry Potter", "lenght": 400, "genre": "fantasy"}
-        ]
-        for book in books_data:
-            Book.objects.create(object=book)
-
-    return response
 
 @app.route("/about",  methods=['GET','POST'])
 def about(request):
@@ -46,14 +31,3 @@ def about(request):
 def about(request, name):
     response.text = f"Hello {name}"
     return response
-
-@app.route("/book")
-class BooksResource:
-    def get(self, req):
-        response.json = {"page":"Books Page"}
-        return response
-    
-    def post(self,req):
-        print(req.json)
-        response.json = req.json
-        return response
