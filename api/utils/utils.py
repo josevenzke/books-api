8079ba90e998bf591bc6f879047a064820254940
JoseVenzke <josevenzkejr@gmail.com>
2023-04-26 18:39:14 -0300

    refactor: added typing

--- api/utils/utils.py
+++ api/utils/utils.py
@@ -1,3 +1,7 @@
+from typing import Union
+from venSQL.model import BaseModel
 
-def deserializer(objects: list) -> list:
-    return [obj.__dict__ for obj in objects]
+def deserializer(objects: Union[BaseModel,list[BaseModel]], many=False) -> list:
+    if many:
+        return [obj.__dict__ for obj in objects]
+    return objects.__dict__
