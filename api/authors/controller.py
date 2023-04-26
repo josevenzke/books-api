4c1b454a97e6bbb73b09a9dc8835870c2b0bd9c8
josevenzke <josevenzkejr@gmail.com>
2023-04-26 14:30:37 -0300

    feature: added GET and POST endpoints to authors

--- api/authors/controller.py
+++ api/authors/controller.py
@@ -1,15 +1,12 @@
 from api.config.app import app, response
 from api.authors import services
 
-@app.route("/book")
+@app.route("/author")
 class AuthorsResource:
     def get(self, req):
         authors = services.get_authors()
-        response.json = {"authors":authors}
-        return response
+        return {"authors":authors}, 200
     
     def post(self,req):
-        print(req.json)
         new_author = services.create_author(req.json)
-        response.json = req.json
-        return response
+        return {"author":new_author}, 201
--- api/authors/services.py
+++ api/authors/services.py
@@ -1,7 +1,11 @@
 from api.authors.model import Author
-from api.utils.utils import deserializer
+from api.utils import utils
 
 def get_authors():
     authors = Author.objects.get_all()
-    result = deserializer(authors)
+    result = utils.deserializer(authors)
     return result
+
+def create_author(data: Author):
+    author = Author.objects.create(data)
+    return utils.deserializer([author])
