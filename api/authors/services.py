73ef2b6f8ac7fddc48cc7c5479d5140ae6dba8ad
JoseVenzke <josevenzkejr@gmail.com>
2023-05-03 22:34:03 -0300

    fix: now using deserializer correctly

--- api/authors/services.py
+++ api/authors/services.py
@@ -3,9 +3,9 @@ from api.utils import utils
 
 def get_authors():
     authors = Author.objects.get_all()
-    result = utils.deserializer(authors)
+    result = utils.deserializer(authors, many=True)
     return result
 
 def create_author(data: Author):
     author = Author.objects.create(data)
-    return utils.deserializer([author])
+    return utils.deserializer(author)
