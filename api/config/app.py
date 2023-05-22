6a9ddb4f5abe4f62422ecdd1acef9a657346e0eb
JoseVenzke <josevenzkejr@gmail.com>
2023-05-22 14:52:36 -0300

    fix: removed testing logs

--- api/config/app.py
+++ api/config/app.py
@@ -4,9 +4,6 @@ from utils.constants import DB_SETTINGS
 from books.model import Book
 
 Controller.connect(database_settings=DB_SETTINGS)
-x = Controller(Book).get_all()
-print(x)
-print(Controller.connection)
 app = Venzpy()
 
 from authors import controller
